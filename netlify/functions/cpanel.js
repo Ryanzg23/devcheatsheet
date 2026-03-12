7a7a99322dd01d303a8fc68bfb02b0d79d1ecf0b
Ryanzg23 <ryanzgregorio@gmail.com>
2026-03-12 12:01:39 +0800

    Update cpanel.js

--- netlify/functions/cpanel.js
+++ netlify/functions/cpanel.js
@@ -1,6 +1,6 @@
 const { neon } = require("@neondatabase/serverless");
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.NETLIFY_DATABASE_URL);
 
 exports.handler = async (event) => {
 
